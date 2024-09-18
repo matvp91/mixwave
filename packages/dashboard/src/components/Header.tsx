9a384685f681b711a2bf10efdbaa1fce88ce9659
Matthias Van Parijs <matvp91@gmail.com>
2024-09-18 20:15:23 +0200

    chore: Removed name

--- packages/dashboard/src/components/Header.tsx
+++ packages/dashboard/src/components/Header.tsx
@@ -7,9 +7,8 @@ export function Header() {
 
   return (
     <div className="p-4 border-b border-border flex h-16">
-      <Link to="/" className="flex gap-2 items-center">
+      <Link to="/" className="flex items-center">
         <img src={logo} className="w-[24px]" />
-        <span className="font-medium">Mixwave</span>
       </Link>
       <div className="flex items-center gap-8 ml-8">
         <Link

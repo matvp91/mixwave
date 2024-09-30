9dbcc0698b7e99a2736d63d0c8ae73ba46598e13
Matthias Van Parijs <matvp91@gmail.com>
2024-09-30 22:21:17 +0200

    Gradually expose segments in a playlist over a schedule

--- packages/stitcher/src/fast.ts
+++ packages/stitcher/src/fast.ts
@@ -6,8 +6,13 @@ import {
   Variant,
   Segment,
 } from "../extern/hls-parser/types.js";
-import { fetchPlaylist } from "./playlist.js";
 import { withPath } from "./uri.js";
+
+// Below is a very minimalistic FAST channel implementation,
+// segments are gradually exposed over time in a seekable window.
+// Not to be taken seriously, it's a PoC!
+
+const schedule = createSchedule();
 
 export async function getFastMasterPlaylist() {
   const master = new MasterPlaylist({
@@ -23,46 +28,117 @@ export async function getFastMasterPlaylist() {
   return hlsParser.stringify(master);
 }
 
-export async function getFastMediaPlaylist(path: string) {
-  const startDate = Date.now() - 5 * /* MS_PER_MINUTE= */ 60000;
+type ScheduleItem = {
+  start: number;
+  end: number;
+  uri: string;
+};
 
-  const items: string[] = [];
-  for (let i = 0; i < 20; i++) {
-    items.push("468ea987-2c3a-4301-9eca-fb89fd26b79b");
+function createSchedule() {
+  // Go back 30m in time
+  const s = Date.now() - 1000 * 60 * 30;
+  const schedule: ScheduleItem[] = [];
+
+  // 30s clips each, 60 minutes in total
+  for (let i = 0; i < 2 * 60; i++) {
+    const start = s + i * 1000 * 30;
+    schedule.push({
+      start,
+      end: start + 30 * 1000,
+      uri: "468ea987-2c3a-4301-9eca-fb89fd26b79b",
+    });
   }
+
+  return schedule;
+}
+
+async function getMediaPlaylist(scheduleItem: ScheduleItem) {
+  const masterFormat = formatUri(scheduleItem.uri);
+  const masterUrl = withPath(masterFormat.base, masterFormat.file);
+  const master = await fetchMemoizedPlaylist<MasterPlaylist>(masterUrl);
+
+  const mediaUrl = withPath(masterFormat.base, master.variants[0].uri);
+  const format = formatUri(mediaUrl);
+  const media = await fetchMemoizedPlaylist<MediaPlaylist>(mediaUrl);
+
+  return { format, media };
+}
+
+export async function getFastMediaPlaylist(path: string) {
+  const now = Date.now();
+
+  const dvrLength = 1000 * 60;
 
   const segments: Segment[] = [];
 
-  for (const item of items) {
-    const format = formatUri(item);
-    const url = withPath(format.base, format.file);
-    const playlist = await fetchPlaylist<MasterPlaylist>(url);
+  let id = 1;
+  let firstBase = 0;
+  let targetDuration = 0;
 
-    const mediaChunkUrl = withPath(format.base, playlist.variants[0].uri);
-    const mediaChunkFormat = formatUri(mediaChunkUrl);
-    const mediaChunk = await fetchPlaylist<MediaPlaylist>(mediaChunkUrl);
+  for (const scheduleItem of schedule) {
+    const { format, media } = await getMediaPlaylist(scheduleItem);
 
-    mediaChunk.segments.forEach((segment, index) => {
-      if (segment.map?.uri === "init.mp4") {
-        segment.map.uri = withPath(mediaChunkFormat.base, segment.map.uri);
+    let prevDuration = 0;
+
+    if (media.targetDuration > targetDuration) {
+      targetDuration = media.targetDuration;
+    }
+
+    media.segments.forEach((segment, index) => {
+      id++;
+
+      const duration = Math.trunc(segment.duration * 1000);
+      const start = scheduleItem.start + prevDuration;
+      const end = start + duration;
+      prevDuration += duration;
+
+      if (start > now || end < now - dvrLength) {
+        return;
       }
 
-      segment.uri = withPath(mediaChunkFormat.base, segment.uri);
+      if (!firstBase) {
+        firstBase = id;
+      }
 
-      if (index === mediaChunk.segments.length - 1) {
+      if (!index) {
+        // If it's the last segment, it's a discontinuity.
         segment.discontinuity = true;
       }
-    });
 
-    segments.push(...mediaChunk.segments);
+      segment.programDateTime = new Date(start);
+
+      if (segment.map?.uri === "init.mp4") {
+        segment.map.uri = withPath(format.base, segment.map.uri);
+      }
+
+      segment.uri = withPath(format.base, segment.uri);
+
+      segments.push(segment);
+    });
   }
 
-  segments[0].programDateTime = new Date(startDate);
+  // segments[0].programDateTime = new Date(pdt);
 
   const media = new MediaPlaylist({
-    targetDuration: 4,
+    targetDuration,
     segments,
   });
 
+  media.mediaSequenceBase = firstBase;
+
   return hlsParser.stringify(media);
+}
+
+const cache = new Map<string, string>();
+
+async function fetchMemoizedPlaylist<T>(url: string) {
+  let text = cache.get(url);
+
+  if (!text) {
+    const response = await fetch(url);
+    text = await response.text();
+    cache.set(url, text);
+  }
+
+  return hlsParser.parse(text) as T;
 }

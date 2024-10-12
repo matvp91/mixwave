5b757181cc7c1be5e5dd3a04b2fce90a23e08392
Matthias Van Parijs <matvp91@gmail.com>
2024-10-12 19:21:54 +0200

    fix: Ignore ts error due to a bug in Elysia eden

--- packages/dashboard/src/api.ts
+++ packages/dashboard/src/api.ts
@@ -3,4 +3,6 @@ import type { App } from "@mixwave/api/client";
 
 export type * from "@mixwave/api/client";
 
+// See https://tezos.stackexchange.com/questions/6380/elysia-eden-treaty-types-error
+// @ts-expect-error
 export const api = treaty<App>(window.__ENV__.PUBLIC_API_ENDPOINT);

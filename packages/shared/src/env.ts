e6a6a3bf69d65956bf6b14bb4a119af0665791f5
Matthias Van Parijs <matvp91@gmail.com>
2024-10-12 19:18:29 +0200

    Load env on demand

--- packages/shared/src/env.ts
+++ packages/shared/src/env.ts
@@ -3,9 +3,17 @@ import { Value } from "@sinclair/typebox/value";
 import findConfig from "find-config";
 import { config } from "dotenv";
 
-const configPath = findConfig("config.env");
-if (configPath) {
-  config({ path: configPath });
+let configEnvLoaded = false;
+
+function loadConfigEnv() {
+  if (configEnvLoaded) {
+    return;
+  }
+  const configPath = findConfig("config.env");
+  if (configPath) {
+    config({ path: configPath });
+  }
+  configEnvLoaded = true;
 }
 
 type ParseEnvResolve<R extends Parameters<typeof t.Object>[0]> = (
@@ -15,6 +23,7 @@ type ParseEnvResolve<R extends Parameters<typeof t.Object>[0]> = (
 export function parseEnv<R extends Parameters<typeof t.Object>[0]>(
   resolve: ParseEnvResolve<R>,
 ) {
+  loadConfigEnv();
   const schema = t.Object(resolve(t));
   return Value.Parse(schema, process.env);
 }

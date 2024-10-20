86ae303a76c320faa08da95c4004a87e7d41803d
Matthias Van Parijs <matvp91@gmail.com>
2024-10-20 09:46:28 +0200

    fix: Facade destroyed due to React strictmode

--- packages/player/src/react/hooks/useController.ts
+++ packages/player/src/react/hooks/useController.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { HlsFacade } from "..";
 import type Hls from "hls.js";
 
@@ -6,27 +6,28 @@ type MediaRefCallback = (media: HTMLMediaElement | null) => void;
 
 export type Controller = ReturnType<typeof createController>;
 
-export function useController(hls: Hls, userFacade?: HlsFacade) {
-  const [facade] = useState<HlsFacade>(() => {
-    if (userFacade) {
-      return userFacade;
-    }
-    return new HlsFacade(hls);
-  });
-
-  const mediaRef = useCallback((media: HTMLMediaElement | null) => {
-    if (media) {
-      hls.attachMedia(media);
-    }
-  }, []);
+export function useController(hls: Hls) {
+  const [facade] = useState<HlsFacade>(() => new HlsFacade(hls));
+  const lastMediaRef = useRef<HTMLMediaElement | null>(null);
 
   useEffect(() => {
-    if (facade) {
-      return () => {
-        facade.destroy();
-      };
-    }
+    return () => {
+      if (document.body.contains(lastMediaRef.current)) {
+        // If element is still in DOM, don't destroy. Omits strict mode too.
+        return;
+      }
+      facade.destroy();
+    };
   }, [facade]);
+
+  const mediaRef = useCallback((media: HTMLMediaElement | null) => {
+    lastMediaRef.current = media;
+    if (media) {
+      hls.attachMedia(media);
+    } else {
+      hls.detachMedia();
+    }
+  }, []);
 
   const controllerRef = useRef<Controller>();
   if (!controllerRef.current) {
